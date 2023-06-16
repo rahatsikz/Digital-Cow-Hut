f2bd875c2af6b85aa23edcd99659256344bac064
rahatsikz <rascal6271@gmail.com>
2023-06-16 17:11:07 +0600

    [DONE] ~ order post api zod valiadation

--- src/app/modules/order/order.route.ts
+++ src/app/modules/order/order.route.ts
@@ -1,8 +1,14 @@
 import express from "express";
 import { OrderController } from "./order.controller";
+import { validateRequest } from "../../middleware/validateRequest";
+import { OrderValidate } from "./order.validate";
 
 const router = express.Router();
 
-router.post("/create-order", OrderController.createOrder);
+router.post(
+  "/create-order",
+  validateRequest(OrderValidate.createOrderZodSchema),
+  OrderController.createOrder
+);
 
 export const OrderRoutes = router;
